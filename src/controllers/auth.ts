d9b510b0d2d847c88978bbc665603880473f2769
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-07 22:47:46 +0100

    added register controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,22 @@
+import catchAsync from "../utils/catchAsync";
+import { Request, Response } from "express";    
+import User from "../models/user";
+
+export const register = catchAsync(async(req: Request, res: Response)=>{
+    const user = await User.create({...req.body})
+    const token = user.genToken();
+    (req.session as unknown as {token: string}).token = token
+    return res.status(201).json({...user.toObject(), password: undefined})
+})
+
+export const login = catchAsync((req: Request, res: Response)=>{
+
+})
+
+export const forgetPassword = catchAsync((req: Request, res: Response)=>{
+
+})
+
+export const resetPassword = catchAsync((req: Request, res: Response)=>{
+
+})
