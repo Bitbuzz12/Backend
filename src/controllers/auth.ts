48b51d035dbefd4440468d88880a1c4d01469d82
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-08 07:09:48 +0100

    added auth/login controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -9,8 +9,17 @@ export const register = catchAsync(async(req: Request, res: Response)=>{
     return res.status(201).json({...user.toObject(), password: undefined})
 })
 
-export const login = catchAsync((req: Request, res: Response)=>{
+export const login = catchAsync(async(req: Request, res: Response)=>{
     const {email, password} = req.body
+    if(!email)return res.status(400).json("email is required")
+    if(!password)return res.status(400).json("password is required")
+    const user = await User.findOne({email})
+    if(!user)return res.status(404).json("email is not registered")
+    const passwordMatch = await user.verifyPassword(password)
+    if(!passwordMatch)return res.status(400).json("incorrect password")
+    const token = user.genToken();
+    (req.session as unknown as {token: string}).token = token
+    return res.status(201).json({...user.toObject(), password: undefined})
 })
 
 export const forgetPassword = catchAsync((req: Request, res: Response)=>{
