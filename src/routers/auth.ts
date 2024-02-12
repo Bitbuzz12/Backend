a82cbc5e3b86de1c920aabf817d2075b9dbd6445
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-12 10:05:54 +0100

    added missing login route

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -4,6 +4,7 @@ import * as auth from "../controllers/auth"
 const router = Router()
 
 router.post("/register", auth.register)
+router.post("/login", auth.login)
 router.post("/forget-password", auth.forgetPassword)
 router.patch("/forget-password/:token", auth.resetPassword)
 
