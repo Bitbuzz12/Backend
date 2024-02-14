d4bbfe4e0ee8cc8f603571d9d4efef505e19f42d
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-14 22:10:23 +0100

    defined new media routes

--- src/routers/media.ts
+++ src/routers/media.ts
@@ -7,5 +7,6 @@ const router = Router()
 router.post("/medias", Media.createMedia)
 router.get("/medias", Media.getMedias)
 router.get("/medias/:mediaId", Media.getMedias)
+router.delete("/medias/:mediaId", Media.deleteMedia)
 
 export default router;
