cddcbe5234ca38c2a14fc2517e4c67447768e5aa
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-14 19:51:09 +0100

    defined medias controllers

---
+++ src/controllers/comment.ts
@@ -0,0 +1,36 @@
+import catchAsync from "../utils/catchAsync";
+import Media from "../models/media";
+import Comment from "../models/comment";
+import { Request, Response } from "express";
+
+export const createMedia = catchAsync(async(req: Request, res: Response, )=>{
+
+})
+
+export const getMyMedias = catchAsync(async(req: Request, res: Response)=>{
+
+})
+
+export const getMedias = catchAsync(async(req: Request, res: Response)=>{
+
+})
+
+export const getMedia = catchAsync(async(req: Request, res: Response)=>{
+
+})
+
+export const deleteMedia = catchAsync(async(req: Request, res: Response)=>{
+
+})
+
+export const reactToMedia = catchAsync(async(req: Request, res: Response)=>{
+
+})
+
+export const commentOnMedia = catchAsync(async(req: Request, res: Response)=>{
+
+})
+
+export const repostMedia = catchAsync(async(req: Request, res: Response)=>{
+    
+})
